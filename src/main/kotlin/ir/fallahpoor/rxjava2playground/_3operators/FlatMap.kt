da9834e9aabaa8ef3f6a07d6b9f1bb091d5852fe
masood <masood.fallahpoor@gmail.com>
2020-05-28 11:57:46 +0430

    Revise FlatMap.kt

--- src/main/kotlin/ir/fallahpoor/rxjava2playground/_3operators/FlatMap.kt
+++ src/main/kotlin/ir/fallahpoor/rxjava2playground/_3operators/FlatMap.kt
@@ -11,22 +11,21 @@ fun main() {
      */
 
     /*
-     * The following solution is highly unsatisfactory because we lose the ability to transform
-     * the data stream.
-     * If we wanted to modify each URL, We'd have to do it all in the Subscriber.
+     * Consider the following solution.
      */
     query("Hello, world!")
         .subscribe { urls: List<String> ->
-            for (url in urls) {
-                println(url)
+            urls.forEach {
+                println(it)
             }
         }
 
     /*
-     * In the following solution We've gotten rid of the 'for' loop, but the resulting code
-     * is a mess. We've got nested subscriptions now. It's ugly and hard to modify.
-     * Remember that one of RxJava advantages was to get rid of nested callbacks but now we're
-     * having nested subscriptions!
+     * The above solution is highly unsatisfactory because we lose the ability to transform
+     * the data stream.
+     * If we wanted to modify each URL, We'd have to do it all in the Subscriber.
+     *
+     * Now take a look at next solution.
      */
     query("Hello, world!")
         .subscribe { urls: List<String> ->
@@ -35,18 +34,12 @@ fun main() {
         }
 
     /*
+     * In the above solution We've gotten rid of the 'for' loop, but the resulting code
+     * is a mess. We've got nested subscriptions now. It's ugly and hard to modify.
+     * Remember that one of RxJava advantages was to get rid of nested callbacks but now
+     * we're having nested subscriptions!
+     *
      * The final solution takes advantage of flatMap() to get rid of nested subscription.
-     *
-     * flatMap() transforms the items emitted by an Observable into Observables,
-     * then flattens the emissions from those into a single Observable.
-     * Conceptually flatMap() takes Observable<T> and a function from T to Observable<R>.
-     * flatMap() first constructs Observable<Observable<R>> replacing all upstream values
-     * of type T with Observable<R> (just like map()). However, it does not stop there: it
-     * automatically subscribes to these inner Observable<R> streams to produce a single
-     * stream of type R, containing all values from all inner streams, as they come.
-     *
-     * flatMap() does NOT guarantee any specific order of events. So interleaving of events of
-     * Observables may occur.
      */
 
     query("Hello, world!")
@@ -54,7 +47,21 @@ fun main() {
         .subscribe { url: String -> println(url) }
 
     /*
-     * The following solution is a better version of above one. It's using the
+     * flatMap() transforms the items emitted by an Observable into Observables,
+     * then flattens the emissions from those into a single Observable.
+     *
+     * Conceptually flatMap() takes Observable<T> and a function from T to Observable<R>.
+     * flatMap() first constructs Observable<Observable<R>> replacing all upstream values
+     * of type T with Observable<R> (just like map()). However, it does not stop there: it
+     * automatically subscribes to these inner Observable<R> streams to produce a single
+     * stream of type R, containing all values from all inner streams, as they come.
+     *
+     * flatMap() does NOT guarantee any specific order of events. So interleaving of events
+     * of Observables may occur.
+     */
+
+    /*
+     * The following solution is a better version of last one. It's using the
      * flatMapIterable operator to get rid of that lengthy flatMap of previous solution.
      */
     query("Hello, world!")
@@ -63,7 +70,7 @@ fun main() {
 
 }
 
-// Returns a list of website URLs based on a text search
+// Returns a hypothetical list of website URLs based on a text search
 fun query(text: String): Observable<List<String>> {
     return Observable.just(listOf("http://www.google.com", "http://www.amazon.com", "http://fallahpoor.ir"))
 }
